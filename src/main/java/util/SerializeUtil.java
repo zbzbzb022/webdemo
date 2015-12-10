b7f8d3366cdbda43bdb6bacf6aeb414c4460a8e8
希龙的囚徒 <zbzbzb022@163.com>
2015-12-10 15:17:35 +0800

    Signed-off-by: 希龙的囚徒 <zbzbzb022@163.com>

---
+++ src/main/java/util/SerializeUtil.java
@@ -0,0 +1,38 @@
+package util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class SerializeUtil {
+	public static byte[] serialize(Object obj) {
+		ObjectOutputStream oos = null;
+		ByteArrayOutputStream baos = null;
+		try {
+			baos = new ByteArrayOutputStream();
+			oos = new ObjectOutputStream(baos);
+			oos.writeObject(obj);
+			byte[] bytes = baos.toByteArray();
+			return bytes;
+		} catch (IOException e) {
+			throw new RuntimeException("对象不可序列化");
+		}
+
+	}
+
+	public static Object unserialize(byte[] bytes) {
+		ByteArrayInputStream bais = null;
+		try {
+			bais = new ByteArrayInputStream(bytes);
+			ObjectInputStream ois = new ObjectInputStream(bais);
+			return ois.readObject();
+		} catch (IOException e) {
+			throw new RuntimeException();
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException();
+		}
+	}
+
+}
